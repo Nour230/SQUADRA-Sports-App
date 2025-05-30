0211bfb9f06b01e77f609ef2a666b70a50bd19ad
nour <nouressam5833@gmail.com>
2025-05-30 09:22:50 +0800

    create local repo

---
+++ SQUADRA-SPORTS-APP/AllSports/Model/AllSportsModel.swift
@@ -0,0 +1,8 @@
+//
+//  AllSportsModel.swift
+//  SQUADRA-SPORTS-APP
+//
+//  Created by nourhan essam on 30/05/2025.
+//
+
+import Foundation
---
+++ SQUADRA-SPORTS-APP/AllSports/Presenter/AllSportsPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  AllSportsPresenter.swift
+//  SQUADRA-SPORTS-APP
+//
+//  Created by nourhan essam on 30/05/2025.
+//
+
+import Foundation
