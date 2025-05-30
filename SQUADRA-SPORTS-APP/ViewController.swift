d7114c664b124783acbe903cefd788aaa8adaad5
nour <nouressam5833@gmail.com>
2025-05-30 09:17:54 +0800

    Initial Commit

---
+++ SQUADRA-SPORTS-APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SQUADRA-SPORTS-APP
+//
+//  Created by nourhan essam on 30/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SQUADRA-SPORTS-APPUITests/SQUADRA_SPORTS_APPUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SQUADRA_SPORTS_APPUITestsLaunchTests.swift
+//  SQUADRA-SPORTS-APPUITests
+//
+//  Created by nourhan essam on 30/05/2025.
+//
+
+import XCTest
+
+final class SQUADRA_SPORTS_APPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
