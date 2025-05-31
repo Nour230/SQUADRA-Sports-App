324576204faa60ac0243311233dc2e9141c4b9c9
Nour230 <Nour342002!>
2025-05-31 21:13:06 +0800

    create league details structure

---
+++ SQUADRA-SPORTS-APP/LeagueDetails/View/Events/EventCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EventCollectionViewCell.swift
+//  SQUADRA-SPORTS-APP
+//
+//  Created by nourhan essam on 31/05/2025.
+//
+
+import UIKit
+
+class EventCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
